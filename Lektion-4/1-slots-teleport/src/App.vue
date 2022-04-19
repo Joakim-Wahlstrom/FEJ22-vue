c12f2f1167bf4e6ebd3da232eebe7bfa6de64ad9
Joakim <wahlstrom.joakim@gmail.com>
2022-04-19 10:41:22 +0200

    lektion 4

---
+++ Lektion-4/1-slots-teleport/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="app">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+    <button @click="showModal2 = true">Show modal2</button>
+
+    <Modal v-if="showModal" @close="showModal = false" />
+
+    <Modal v-if="showModal2" @close="showModal2 = false">
+      <template v-slot:links>
+        <a href="#">My website</a>
+        <a href="#">Google</a>
+      </template>
+      <form>
+        <h2>Login</h2>
+        <label class="input-group">
+          <p>Email: </p>
+          <input type="email" class="form-control">
+        </label>
+        <label class="input-group">
+          <p>Password: </p>
+          <input type="password" class="form-control">
+        </label>
+      </form>
+      <button @click="showModal2 = false">Close Modal</button>
+    </Modal>
+  </div>
+  <a href="#">Yahoo</a>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false,
+      showModal2: false,
+    }
+  }
+}
+</script>
+
+<style>
+#app, #modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* .app {
+  position: relative;
+  right: 10rem;
+} */
+button {
+  padding: .6em 1.2em;
+  border: 0;
+  border-radius: .5em;
+  cursor: pointer;
+  margin: 1rem;
+  background-color: orangered;
+  color: #fff;
+}
+</style>
---
+++ Lektion-4/1-slots-teleport/src/components/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+  <teleport to="#modals">
+    <div class="modal-wrapper" @click.self="$emit('close')">
+      <div class="modal">
+        <slot>
+          <h2>Cookies</h2>
+          <p>vi sparar all din data och säljer till högstbjudande..</p>
+          <button @click="$emit('close')">Close</button>
+        </slot>
+        <hr>
+        <slot name="links">
+          <!-- <a href="#">My website</a>
+          <a href="#">Google</a> -->
+        </slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: 'ModalComponent'
+}
+</script>
+
+<style>
+  .modal-wrapper {
+    background-color: #00000053;
+    position: absolute;
+    inset: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .modal {
+    background: #fff;
+    padding: 3rem;
+    border-radius: 1em;
+    box-shadow: 5px 10px 1rem #11111130, 5px 10px 5rem #33333316 ;
+  }
+  .input-group {
+    margin-bottom: 2rem;
+
+  }
+  .form-control {
+    width: 100%;
+    border-radius: .5em;
+    border: 1px solid #333;
+    padding: .4em .6em;
+  }
+  .modal a {
+    text-decoration: none;
+    margin: 1rem;
+  }
+</style>

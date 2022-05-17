f2a879aeb5d0b8cd13fd7cae7711ba16d39b9de2
Joakim <wahlstrom.joakim@gmail.com>
2022-05-17 14:16:26 +0200

    lektion 9

---
+++ Lektion-9/2-composition-todo/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todo">
+    <p class="title">{{ todo.title }}</p>
+    <button>X</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

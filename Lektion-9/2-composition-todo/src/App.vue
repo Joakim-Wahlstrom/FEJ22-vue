04125606c41f2331b0fdfebf3f866443037b261f
Joakim <wahlstrom.joakim@gmail.com>
2022-05-17 13:42:12 +0200

    Lektion 9

---
+++ Lektion-9/2-composition-todo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <AddTodoForm />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import AddTodoForm from './components/AddTodoForm.vue'
+export default {
+  components: {
+    AddTodoForm
+  },
+  setup() {
+    const todos = ref([
+      { id: 1, title: 'Todo 1', completed: false },
+      { id: 2, title: 'Todo 2', completed: true },
+      { id: 3, title: 'Todo 3', completed: false }
+    ])
+
+    return { todos }
+  }
+}
+</script>
+
+<style>
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+ }
+ .container {
+   margin: 5% 15%;
+ }
+</style>
---
+++ Lektion-9/2-composition-todo/src/components/AddTodoForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="form" @submit.prevent="handleSubmit">
+    <input type="text" placeholder="Add todo" v-model="title">
+    <button>Add</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const title = ref('')
+
+    return { title }
+  }
+}
+</script>
+
+<style scoped>
+  .form {
+    margin-bottom: 2rem;
+    display: flex;
+  }
+  input {
+    flex: 1;
+    padding: .6rem;
+    border-radius: .5rem;
+    border: 1px solid #cecece;
+  }
+  input:focus {
+    outline: 1px solid #818181;
+  }
+  button {
+    padding: .5rem .7rem;
+    background-color: #333;
+    color: #fff;
+    border: 0;
+    border-radius: .5rem;
+    margin-left: 2rem;
+    cursor: pointer;
+  }
+</style>
